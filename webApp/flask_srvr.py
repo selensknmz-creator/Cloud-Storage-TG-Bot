92c022e25da41a54e54f3212d67931ac348484fc
selensknmz-creator <selensknmz@gmail.com>
2026-03-31 20:35:32 +0530

    New port assigned

--- webApp/flask_srvr.py
+++ webApp/flask_srvr.py
@@ -7,4 +7,4 @@ def home():
     return jsonify({"message": "Flask server running!"})
 
 def run_flask():
-    app.run(host="0.0.0.0", port=7860, debug=False, use_reloader=False)
+    app.run(host="0.0.0.0", port=8501, debug=False, use_reloader=False)
